a302733aa27a0f0949cfc3585c3cd7526e4a6caa
jikwan <jikwan0327@gmail.com>
2022-06-08 08:18:14 +0900

    0608TIL

--- memo/js로 크롭앱만들기(노마더)/#4. LOGIN/#4.6 Loading Username/index.js
+++ memo/js로 크롭앱만들기(노마더)/#4. LOGIN/#4.6 Loading Username/index.js
@@ -5,16 +5,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 //css를 이용해 숨겨줌
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     //입력창에 입력한 값을 출력하는 함수
     event.preventDefault(); //브라우저의 기본동작을 막아줌
     loginForm.classList.add("hidden"); //로그인창을 숨김
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME); //숨어있던 h1태그를 보이게함
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 //loginButton을 클릭 하였을 때 onLoginBtnClick함수 호출
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(username);
+}
