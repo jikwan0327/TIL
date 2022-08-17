f3cd00f8795568bcf3f03073442bc59d42a027e4
jikwan <jikwan0327@gmail.com>
2022-08-18 00:01:32 +0900

    ToDos with typeScript

---
+++ memo/Nomadcoder/react-masterClass/Todo/src/components/ToDo.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "Done" && (
+        <button name="Done" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
---
+++ memo/Nomadcoder/react-masterClass/Todo/src/components/ToDoList.tsx
@@ -0,0 +1,23 @@
+import { useRecoilValue } from "recoil";
+import { toDoState } from "./atoms";
+import CreateToDO from "./CreateToDo";
+import ToDo from "./ToDo";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDO />
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
