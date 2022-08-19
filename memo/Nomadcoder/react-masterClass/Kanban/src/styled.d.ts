d760042f8840db49e68365d995791df94bde6a1d
jikwan <jikwan0327@gmail.com>
2022-08-19 23:45:03 +0900

    SVG animation

--- memo/Nomadcoder/react-masterClass/Kanban/src/styled.d.ts
+++ memo/Nomadcoder/react-masterClass/Kanban/src/styled.d.ts
@@ -1,10 +1,5 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme {
-    bgColor: string;
-    textColor: string;
-    accentColor: string;
-    cardBgColor: string;
-  }
+  export interface DefaultTheme {}
 }
