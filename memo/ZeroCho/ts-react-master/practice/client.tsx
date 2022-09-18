d532816241b8f7d8c11695f7b4cdc576f5fa28d4
jikwan <jikwan0327@gmail.com>
2022-09-18 21:25:36 +0900

    ResponseCheck

--- memo/ZeroCho/ts-react-master/practice/client.tsx
+++ memo/ZeroCho/ts-react-master/practice/client.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import NumberBaseball from "./NumberBaseball";
+import ResponseCheck from "./ResponseCheck";
 
-ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
+ReactDOM.render(<ResponseCheck />, document.querySelector("#root"));
